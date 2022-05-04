fbd15e07bf80a9989f07fd4a93d492614a76344a
kfirfer <kfirfer@gmail.com>
2022-05-04 12:35:33 +0300

    wip

--- consumer/client.go
+++ consumer/client.go
@@ -1,45 +1,11 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"fmt"
 	"github.com/nats-io/nats.go"
-	"github.com/novalagung/natskeepalivesubscribe"
-	"strings"
 	"time"
 )
-
-func Start() error {
-	natsURL := "nats://localhost:4222"
-	subject := "messages"
-	natskeepalivesubscribe.KeepAliveSubscribe(natsURL, subject, func(msg *nats.Msg) (interface{}, error) {
-		// parse payload
-		payload := make(map[string]interface{})
-		err := json.Unmarshal(msg.Data, &payload)
-		if err != nil {
-			return nil, err
-		}
-
-		// handle the request
-		switch strings.ToUpper(payload["method"].(string)) {
-		case "OPTIONS":
-			// ...
-		case "GET":
-			// ...
-		case "POST":
-			fmt.Println(payload["message"])
-			msg.Reply = "ok"
-		case "PATCH":
-			// ...
-		case "PUT":
-			// ...
-		case "DELETE":
-			// ...
-		}
-		return nil, fmt.Errorf("invalid http method")
-	})
-	return nil
-}
 
 func main() {
 	nc, _ := nats.Connect(nats.DefaultURL, nats.RetryOnFailedConnect(true),
@@ -48,44 +14,20 @@ func main() {
 	)
 	js, _ := nc.JetStream(nats.PublishAsyncMaxPending(256))
 
-	_, _ = js.Subscribe("ORDERS.*", func(msg *nats.Msg) {
-		fmt.Printf("Received a JetStream message: %s\n", string(msg.Data))
-		err := msg.Ack()
-		if err != nil {
-			fmt.Println(err)
+	sub, _ := js.PullSubscribe("ORDERS.*", "order-review", nats.PullMaxWaiting(128))
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
 		}
-	})
-	// Simple Sync Durable Consumer (optional SubOpts at the end)
-
-	//sub, _ := js.SubscribeSync("ORDERS.*", nats.Durable("ORDERS"), nats.MaxDeliver(3))
-	//m, _ := sub.NextMsg(20)
-	//fmt.Println(m)
-	// Simple Pull Consumer
-	//sub, _ := js.PullSubscribe("ORDERS.*", "ORDERS")
-	//msgs, _ := sub.Fetch(10)
-	//fmt.Println(msgs)
-	// Unsubscribe
-	//sub.Unsubscribe()
-	//
-	//// Drain
-	//sub.Drain()
-
-	//if err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-
-	for true {
-		//sub, _ := js.SubscribeSync("ORDERS.*", nats.Durable("ORDERS"), nats.MaxDeliver(3))
-		//m, _ := sub.NextMsg(60)
-		//fmt.Println(m)
-
-		time.Sleep(time.Millisecond * 100)
+		msgs, _ := sub.Fetch(10, nats.Context(ctx))
+		for _, msg := range msgs {
+			msg.Ack()
+			fmt.Println(string(msg.Data[:]))
+		}
 	}
-	// Tests
-	//natsErr := Start()
-	//if natsErr != nil {
-	//	fmt.Println(natsErr)
-	//	return
-	//}
 }
